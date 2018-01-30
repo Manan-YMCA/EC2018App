10db782d985c23cf332e89f77dcb2321bfb569de
Shubham sharma <sharma.shubham0008@gmail.com>
2018-01-31 00:40:31 +0530

    Drawables added Font added Background Images added

--- app/src/main/java/com/example/yatindhingra/ec2k18/CatgoryDashboardActivity.java
+++ app/src/main/java/com/example/yatindhingra/ec2k18/CatgoryDashboardActivity.java
@@ -2,6 +2,7 @@ package com.example.yatindhingra.ec2k18;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CatgoryDashboardActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class CatgoryDashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catgory_dashboard);
+
+
+
     }
 }
