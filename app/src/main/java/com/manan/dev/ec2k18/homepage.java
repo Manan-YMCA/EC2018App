320e2ab7b0f1a878f943ae483150a121c983af2d
gkashish <gkashish004@gmail.com>
2018-01-31 19:34:42 +0530

    out of memory error fixed

--- app/src/main/java/com/manan/dev/ec2k18/homepage.java
+++ app/src/main/java/com/manan/dev/ec2k18/homepage.java
@@ -1,5 +1,8 @@
-package com.example.yatindhingra.ec2k18;
+package com.manan.dev.ec2k18;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -32,10 +35,13 @@ public class homepage extends AppCompatActivity {
         tv4.setVisibility(View.GONE);
         bt1.setVisibility(View.GONE);
         ig.setVisibility(View.GONE);
+
         final NewtonCradleLoading newtonCradleLoading;
         newtonCradleLoading = (NewtonCradleLoading)findViewById(R.id.newton_cradle_loading);
         newtonCradleLoading.start();
         newtonCradleLoading.setLoadingColor(R.color.white);
+        ig.setImageBitmap(
+                decodeSampledBitmapFromResource(getResources(), R.drawable.ec_logo, 250, 250));
         final Handler handler=new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -53,10 +59,48 @@ public class homepage extends AppCompatActivity {
                 bt1.animate().alpha(1.0f).setDuration(1000);
                 ig.setVisibility(View.VISIBLE);
                 ig.animate().alpha(1.0f).setDuration(1000);
+
                 newtonCradleLoading.stop();
 
             }
         },3000);
+    }
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 
 }
