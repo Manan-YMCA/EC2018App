4fe4d7856ce7165443ca2851c7e3ee5fd727d6b3
Shubham Kachroo <shubhamkachroo@yahoo.co.in>
2018-02-01 15:57:26 +0530

    Single Event Activity and layout added and half done.

---
+++ app/src/main/java/com/manan/dev/ec2018app/SingleEventActivity.java
@@ -0,0 +1,13 @@
+package com.manan.dev.ec2018app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingleEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_event);
+    }
+}
