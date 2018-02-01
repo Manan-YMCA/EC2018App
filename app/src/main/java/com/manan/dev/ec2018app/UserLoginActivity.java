58fb1911054a446267dc450927c3632130b56dac
YATIN DHINGRA <yatind01@gmail.com>
2018-02-01 23:25:14 +0530

    FadeIn animation added to all widgets in login activity, minor ui improvements in login activity, activity to verify user mobile number added

--- app/src/main/java/com/manan/dev/ec2018app/UserLoginActivity.java
+++ app/src/main/java/com/manan/dev/ec2018app/UserLoginActivity.java
@@ -1,5 +1,7 @@
 package com.manan.dev.ec2018app;
 
+import android.animation.Animator;
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,28 +26,56 @@ public class UserLoginActivity extends AppCompatActivity {
         final NewtonCradleLoading newtonCradleLoading;
         newtonCradleLoading=(NewtonCradleLoading)findViewById(R.id.newton_cradle_loading);
         newtonCradleLoading.start();
-        newtonCradleLoading.setLoadingColor(R.color.colorPrimaryDark);
         ECText=(TextView)this.findViewById(R.id.ECText);
         ContinueText=(TextView)this.findViewById(R.id.ContinueText);
         GuestText=(TextView)this.findViewById(R.id.GuestText);
         ReadyText=(TextView)this.findViewById(R.id.ReadyText);
         LoginButton=(Button)this.findViewById(R.id.LoginButton);
         ECLogo=(ImageView)this.findViewById(R.id.EClogo);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+                //facebook login here
+                startActivity(new Intent(UserLoginActivity.this, OtpVerificationActivity.class));
+            }
+        });
+
+        ECText.setAlpha(0f);
+        ECText.setVisibility(View.INVISIBLE);
+        ContinueText.setAlpha(0f);
+        ContinueText.setVisibility(View.INVISIBLE);
+        GuestText.setAlpha(0f);
+        GuestText.setVisibility(View.INVISIBLE);
+        ReadyText.setAlpha(0f);
+        ReadyText.setVisibility(View.INVISIBLE);
+        LoginButton.setAlpha(0f);
+        LoginButton.setVisibility(View.INVISIBLE);
+        ECLogo.setAlpha(0f);
+        ECLogo.setVisibility(View.INVISIBLE);
+
         final Handler handler=new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-
-                ECText.setVisibility(View.VISIBLE);
-                ContinueText.setVisibility(View.VISIBLE);
-                GuestText.setVisibility(View.VISIBLE);
-                ReadyText.setVisibility(View.VISIBLE);
-                LoginButton.setVisibility(View.VISIBLE);
-                ECLogo.setVisibility(View.VISIBLE);
                 newtonCradleLoading.stop();
+                ECText.setVisibility(View.VISIBLE);
+                ECText.animate().alpha(1.0f).setDuration(1500).setListener(null);
+                ContinueText.setVisibility(View.VISIBLE);
+                ContinueText.animate().alpha(1.0f).setDuration(1500).setListener(null);
+                GuestText.setVisibility(View.VISIBLE);
+                GuestText.animate().alpha(1.0f).setDuration(1500).setListener(null);
+                ReadyText.setVisibility(View.VISIBLE);
+                ReadyText.animate().alpha(1.0f).setDuration(1500).setListener(null);
+                LoginButton.setVisibility(View.VISIBLE);
+                LoginButton.animate().alpha(1.0f).setDuration(1500).setListener(null);
+                ECLogo.setVisibility(View.VISIBLE);
+                ECLogo.animate().alpha(1.0f).setDuration(1500).setListener(null);
+
 
             }
-        },3000);
+        },2500);
 
     }
 }
