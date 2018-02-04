2a5b04b9ff394f8119fd26852287b888765ad040
gkashish <gkashish004@gmail.com>
2018-02-04 18:42:57 +0530

    Added recycler view to leaderboard fragment

---
+++ app/src/main/java/com/manan/dev/ec2018app/Xunbao/LeaderboardAdapter.java
@@ -0,0 +1,61 @@
+package com.manan.dev.ec2018app.Xunbao;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.manan.dev.ec2018app.R;
+
+import java.util.List;
+
+/**
+ * Created by KASHISH on 04-02-2018.
+ */
+
+public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.MyViewHolder> {
+
+    private List<LeaderboardList> leaderboardList;
+    private Context context;
+
+    public LeaderboardAdapter(Context context, List<LeaderboardList> ll){
+        this.context=context;
+        this.leaderboardList=ll;
+    }
+
+    @Override
+    public LeaderboardAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_leaderboard, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(LeaderboardAdapter.MyViewHolder holder, int position) {
+        LeaderboardList member = leaderboardList.get(position);
+        holder.rank.setText(member.getRank());
+        holder.name1.setText(member.getName1());
+        holder.level.setText(member.getLevel());
+    }
+
+    @Override
+    public int getItemCount() {
+        return leaderboardList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView rank,name1,level;
+
+        public MyViewHolder(View itemLayoutView) {
+            super(itemLayoutView);
+            rank =(TextView) itemLayoutView.findViewById(R.id.Rank);
+            name1=(TextView) itemLayoutView.findViewById(R.id.name);
+            level=(TextView) itemLayoutView.findViewById(R.id.level);
+        }
+    }
+
+}
+
--- app/src/main/java/com/manan/dev/ec2018app/Xunbao/LeaderboardFragment.java
+++ app/src/main/java/com/manan/dev/ec2018app/Xunbao/LeaderboardFragment.java
@@ -3,16 +3,23 @@ package com.manan.dev.ec2018app.Xunbao;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.manan.dev.ec2018app.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class LeaderboardFragment extends Fragment {
 
-
+    private List<LeaderboardList> leaderboardList = new ArrayList<>();
+    RecyclerView recyclerView;
+    LeaderboardAdapter leaderboardAdapter;
     public LeaderboardFragment() {
         // Required empty public constructor
     }
@@ -21,7 +28,17 @@ public class LeaderboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        LeaderboardList dummy = new LeaderboardList("KASHISH","1.","10");
+        leaderboardList.add(dummy);
+        dummy = new LeaderboardList("K","2.","9");
+        leaderboardList.add(dummy);
+        View view =inflater.inflate(R.layout.fragment_leaderboard,container,false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+        leaderboardAdapter = new LeaderboardAdapter(getContext(),leaderboardList);
+        recyclerView.setAdapter(leaderboardAdapter);
+        return view;
     }
 
 }
