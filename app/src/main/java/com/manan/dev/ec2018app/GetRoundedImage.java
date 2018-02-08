1b0e5240cd9877a2af146633564903a383dfd820
Prerna1 <prernasuneja52.ps@gmail.com>
2018-02-09 01:57:05 +0530

    ProfileActivity added

---
+++ app/src/main/java/com/manan/dev/ec2018app/GetRoundedImage.java
@@ -0,0 +1,37 @@
+package com.manan.dev.ec2018app;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.Rect;
+
+/**
+ * Created by Suneja's on 08-02-2018.
+ */
+
+public class GetRoundedImage {
+    public Bitmap getRoundedShape(Bitmap scaleBitmapImage) {
+        // TODO Auto-generated method stub
+        int targetWidth = 300;
+        int targetHeight = 300;
+        Bitmap targetBitmap = Bitmap.createBitmap(targetWidth,
+                targetHeight,Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(targetBitmap);
+        Path path = new Path();
+        path.addCircle(((float) targetWidth - 1) / 2,
+                ((float) targetHeight - 1) / 2,
+                (Math.min(((float) targetWidth),
+                        ((float) targetHeight)) / 2),
+                Path.Direction.CCW);
+
+        canvas.clipPath(path);
+        Bitmap sourceBitmap = scaleBitmapImage;
+        canvas.drawBitmap(sourceBitmap,
+                new Rect(0, 0, sourceBitmap.getWidth(),
+                        sourceBitmap.getHeight()),
+                new Rect(0, 0, targetWidth,
+                        targetHeight), null);
+        return targetBitmap;
+    }
+}
