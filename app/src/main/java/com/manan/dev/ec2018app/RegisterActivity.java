ee86da0fb2b4a25f365c60f3f953333e5e9e9293
YATIN DHINGRA <yatind01@gmail.com>
2018-02-15 15:36:49 +0530

    ui improvements in registration activity, checkers improved

--- app/src/main/java/com/manan/dev/ec2018app/RegisterActivity.java
+++ app/src/main/java/com/manan/dev/ec2018app/RegisterActivity.java
@@ -1,84 +1,94 @@
 package com.manan.dev.ec2018app;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText userName, userEmail, userPhone, userCollege;
+    private RelativeLayout view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        RelativeLayout rl_button = (RelativeLayout)findViewById(R.id.hint_set) ;
-        rl_button.setOnClickListener(new View.OnClickListener() {
+
+        userName = (EditText) findViewById(R.id.et_reg_name);
+        userEmail = (EditText) findViewById(R.id.et_reg_email);
+        userCollege = (EditText) findViewById(R.id.et_reg_clg);
+        userPhone = (EditText) findViewById(R.id.et_reg_mob);
+        view = (RelativeLayout) findViewById(R.id.rl_main_view);
+
+        TextView submit = (TextView) findViewById(R.id.tv_reg_submit);
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                EditText editTextemail = (EditText)findViewById(R.id.email);
-                if( editTextemail.getText().toString().length() == 0 )
-                    editTextemail.setError("Email is required!" );
-                EditText editTextname = (EditText)findViewById(R.id.name);
-                if(editTextname.getText().toString().length() == 0)
-                    editTextname.setError("Name is required!" );
-                EditText editTextmobile = (EditText)findViewById(R.id.mob);
-                if (editTextmobile.getText().length() ==0 )
-                    editTextmobile.setError("Mobile number is required!" );
-                EditText editTextcollege = (EditText)findViewById(R.id.clg);
-                if (editTextcollege.getText().toString().length() ==0 )
-                    editTextcollege.setError("Mobile number is required!" );
-                boolean check;
-                Pattern p;
-                Matcher m;
-
-                String EMAIL_STRING = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-
-                p = Pattern.compile(EMAIL_STRING);
-
-                m = p.matcher(editTextemail.getText().toString());
-                check = m.matches();
-
-                if(!check) {
-                    editTextemail.setError("Not Valid Email");
-                }
-                String mobile = editTextmobile.getText().toString();
-boolean check1 = isValidPhone(mobile);
-                if(!check1) {
-                    editTextmobile.setError("Not Valid Phone no.");
+                Boolean checker = validateCredentials();
+                if(checker){
+                    //TODO
+                    //first check OTP
+                    //the register the user
                 }
             }
         });
 
     }
 
-    private boolean isValidPhone(String phone) {
-
-            boolean check=false;
-            if(!Pattern.matches("[a-zA-Z]+", phone))
-            {
-                if(phone.length() < 6 || phone.length() > 13)
-                {
-                    check = false;
-
-                }
-                else
-                {
-                    check = true;
-
-                }
-            }
-            else
-            {
-                check=false;
-            }
-            return check;
+    private Boolean validateCredentials() {
+        if(!isNetworkAvailable()){
+            Snackbar.make(view, "Check your Internet Connection", Snackbar.LENGTH_SHORT).show();
+            return false;
         }
+        if(userName.getText().toString().equals("")){
+            userName.setError("Enter a User Name");
+            return false;
+        }
+        if(userEmail.getText().toString().equals("")){
+            userEmail.setError("Enter an Email Address");
+            return false;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(userEmail.getText().toString()).matches()){
+            userEmail.setError("Enter a Valid Email Address");
+            return false;
+        }
+        if(userPhone.getText().toString().equals("")){
+            userPhone.setError("Enter a Phone Number");
+            return false;
+        }
+        if(!Patterns.PHONE.matcher(userPhone.getText().toString()).matches()){
+            userPhone.setError("Enter a valid Phone Number");
+            return false;
+        }
+        if(userPhone.getText().toString().length() != 10){
+            userPhone.setError("Enter a valid Phone Number");
+            return false;
+        }
+        if(userCollege.getText().toString().equals("")){
+            userCollege.setError("Enter a College Name");
+            return false;
+        }
+        return true;
     }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+}
 
