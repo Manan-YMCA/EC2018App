fd7b4d52ebb6dd00d3039394caf753a711fbaf19
Prerna1 <prernasuneja52.ps@gmail.com>
2018-02-17 01:20:41 +0530

    UserDetails addeds to ProfileActivity

--- app/src/main/java/com/manan/dev/ec2018app/ProfileActivity.java
+++ app/src/main/java/com/manan/dev/ec2018app/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.manan.dev.ec2018app;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -15,15 +16,32 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.manan.dev.ec2018app.Models.UserDetails;
 import com.manan.dev.ec2018app.Utilities.GetRoundedImage;
 
+import org.json.JSONObject;
+
 public class ProfileActivity extends AppCompatActivity {
+    private UserDetails userDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        SharedPreferences prefs = getSharedPreferences(getResources().getString(R.string.sharedPrefName), MODE_PRIVATE);
+        final String phoneNumber = prefs.getString("Phone", null);
+        if (phoneNumber == null) {
+            Toast.makeText(this, "shared pref no data", Toast.LENGTH_SHORT).show();
+        }
+        getDetails(phoneNumber);
         RelativeLayout profilePicture = (RelativeLayout) findViewById(R.id.profile_picture_layout);
         Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.dashboard_image);
         Bitmap resized = Bitmap.createScaledBitmap(largeIcon, 100, 100, true);
@@ -97,4 +115,47 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
     }
+    private void getDetails(final String phone) {
+      final TextView tvName = (TextView)findViewById(R.id.tv_name);
+        final TextView tvMail=(TextView)findViewById(R.id.tv_email);
+        final TextView tvCollege = (TextView)findViewById(R.id.tv_college);
+        final TextView tvPhone = (TextView)findViewById(R.id.tv_phone);
+        String url = getResources().getString(R.string.get_user_details_api)+phone;
+        Toast.makeText(this, "URL: " + url, Toast.LENGTH_LONG).show();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest obreq = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj1 = new JSONObject(response);
+                            JSONObject obj = obj1.getJSONObject("data");
+                            Toast.makeText(ProfileActivity.this, obj.getString("name"), Toast.LENGTH_LONG).show();
+                            tvName.setText(obj.getString("name"));
+                            tvMail.setText(obj.getString("email"));
+                            tvCollege.setText(obj.getString("college"));
+                            tvPhone.setText(phone);
+
+                        }
+                        // Try and catch are included to handle any errors due to JSON
+                        catch (Exception e) {
+                            // If an error occurs, this prints the error to the log
+//                            Toast.makeText(FBLoginActivity.this, "Error aagya1", Toast.LENGTH_SHORT).show();
+                            e.printStackTrace();
+                            Toast.makeText(ProfileActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    // Handles errors that occur due to Volley
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(ProfileActivity.this, "Error aagya2", Toast.LENGTH_SHORT).show();
+                        Log.e("Volley", "Error");
+                    }
+                }
+        );
+        queue.add(obreq);
+    }
+
 }
