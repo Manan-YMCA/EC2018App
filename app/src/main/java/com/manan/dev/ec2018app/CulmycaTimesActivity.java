ae869dfe3ac73c7d2564338b3d8df5f43d73224f
gkashish <gkashish004@gmail.com>
2018-03-11 21:58:20 +0530

    added like functionality and other fixes

--- app/src/main/java/com/manan/dev/ec2018app/CulmycaTimesActivity.java
+++ app/src/main/java/com/manan/dev/ec2018app/CulmycaTimesActivity.java
@@ -1,6 +1,15 @@
 package com.manan.dev.ec2018app;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.FragmentManager;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,8 +22,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.manan.dev.ec2018app.Adapters.CTAdapter;
+import com.manan.dev.ec2018app.Fragments.FragmentFbLogin;
 import com.manan.dev.ec2018app.Models.approval;
 import com.manan.dev.ec2018app.Models.commentsModel;
+import com.manan.dev.ec2018app.Models.likesModel;
 import com.manan.dev.ec2018app.Models.postsModel;
 import com.manan.dev.ec2018app.R;
 
@@ -31,6 +42,8 @@ public class CulmycaTimesActivity extends AppCompatActivity {
     ProgressDialog progressBar;
     private CTAdapter mAdapter;
 
+    public static AlertDialog.Builder builder;
+    public static AlertDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +55,41 @@ public class CulmycaTimesActivity extends AppCompatActivity {
         progressBar.setCanceledOnTouchOutside(false);
         progressBar.show();
         postReference = FirebaseDatabase.getInstance().getReference("posts");
+
+
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Login Required!");
+        builder.setMessage("To like or share, you must login with facebook");
+        builder.setPositiveButton("Continue", new Dialog.OnClickListener(){
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                SharedPreferences preferences = getSharedPreferences(getResources().getString(R.string.sharedPrefName), Context.MODE_PRIVATE);
+                final String phoneNumber = preferences.getString("Phone", null);
+                if (phoneNumber == null) {
+                    finish();
+                    startActivity(new Intent(CulmycaTimesActivity.this, LoginActivity.class));
+                }
+                else{
+                    FragmentManager fm = getFragmentManager();
+                    FragmentFbLogin fbLogin = new FragmentFbLogin();
+                    fbLogin.show(fm, "fbLoginFragment");
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new Dialog.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        dialog = builder.create();
+
+
+
         postReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -49,8 +97,10 @@ public class CulmycaTimesActivity extends AppCompatActivity {
                     String clubName=club.getKey();
                     Log.v("hey",clubName);
                     for(DataSnapshot posts:club.getChildren()){
+
                         postsModel post=posts.getValue(postsModel.class);
                         post.clubName=clubName;
+                        post.postid=posts.getKey();
                         List<commentsModel> allcomments=new ArrayList<commentsModel>();
                         for(DataSnapshot comments: posts.child("comments").getChildren()) {
                             Log.v("hey",comments.toString());
@@ -58,7 +108,16 @@ public class CulmycaTimesActivity extends AppCompatActivity {
                             Log.v("hey",comment.username);
                             allcomments.add(comment);
                         }
+
                         post.comments=allcomments;
+
+                        List<likesModel> alllikes=new ArrayList<likesModel>();
+                        for(DataSnapshot mlikes: posts.child("likesfid").getChildren()) {
+                            likesModel l = mlikes.getValue(likesModel.class);
+                            alllikes.add(l);
+                        }
+
+                        post.likefids=alllikes;
                         Log.v("hey",allcomments.toString());
                         allposts.add(post);
                     }
